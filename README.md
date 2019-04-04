ea70c15b840d378bccaf26504a9a64fb9671b901
Magnus Månsson <magma@1447.se>
2019-04-04 04:23:36 +0200

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodejs-mqtt-data-logger
+**THIS IS A WORK IN PROGRESS**
+
+## What is this?
+Logs sensor data published to MQTT.
+Handles counters that resets themselves.
+Stores data in sqlite.
+Builtin webserver to show data.
+
+## Links
+https://www.npmjs.com/package/mqtt
+https://www.npmjs.com/package/mqtt-match
+https://www.npmjs.com/package/sqlite3
+https://www.w3schools.com/nodejs/nodejs_http.asp
+https://www.npmjs.com/package/highcharts
---
+++ nodejs-mqtt-data-logger.js
@@ -0,0 +1,63 @@
+#!/usr/bin/nodejs
+
+// Config
+var config = {
+	mqtt: 'mqtt://ha-01.localnet:1883',
+	topics: [
+		{ topic: 'shellies/+/relay/+/energy', multiplier: 0.01, unit: 'kWh', resetingCounter: true }
+	]
+}
+
+
+
+function GetMqttTopics() {
+	var topics = [];
+	config.topics.forEach(function(topic) {
+		topics.push(topic.topic);
+	})
+	return topics
+}
+
+function GetMatchingTopic(topic) {
+	var match = null;
+	config.topics.forEach(function(topic2, index) {
+		if(mqttMatch(topic2.topic, topic)) {
+			match = topic2;
+		}
+	})
+	return match;
+}
+
+
+
+// Main
+var mqtt = require('mqtt');
+var mqttMatch = require('mqtt-match');
+var db = require('sqlite3').verbose();
+
+
+var mqttClient = mqtt.connect(config.mqtt);
+
+mqttClient.on('connect', function() {
+	mqttClient.subscribe(GetMqttTopics(), function(err) {
+		if(!err) {
+			console.info('Connected to MQTT');
+		}
+	})
+})
+
+mqttClient.on('message', function(topic, value) {
+	var match = GetMatchingTopic(topic);
+
+	value = Number(value);
+	if(value !== 'NaN') {
+		if(typeof(match.multiplier) !== 'undefined') {
+			value = value * match.multiplier;
+		}
+		console.info(topic + ': ' + value + ' ' + match.unit);
+	}
+})
+
+
+
+// mqttClient.end()
