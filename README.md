e97f89b18b433b3677cf2b7aedd7f90629cb327c
Magnus Månsson <magma@1447.se>
2019-04-06 17:31:55 +0200

    Aggregating hourly data Purging raw data after X days

--- README.md
+++ README.md
@@ -9,16 +9,19 @@ I was looking for a decent way to long-term log my energy usage for some outlets
 This should run as a daemon and will then read data from a MQTT Broker to which energy usage is sent. The read data will then be stored into a database (SQLite). Only changes will be written and on an hourly basis it will aggregate energy usage into daily values. Detailed (raw) data entries will be deleted after a defined number of days.
 
 # TODO list
-- [ ] Log data to SQLite.
+- [x] Log data to SQLite.
 - [x] Handle counters from hardware that resets the value from time to time (Shelly for example).
 - [ ] Add HTTP support to view the data.
-- [ ] Remove detailed (raw) data after X number of days.
+- [x] Aggregate hourly data into another database table.
 - [ ] Aggregate daily data into another database table.
+- [x] Remove detailed (raw) data after X number of days.
+- [ ] Remove data from hourly/daily as well.
 
 ## Dependencies
 * [mqtt](https://www.npmjs.com/package/mqtt)
 * [mqtt-match](https://www.npmjs.com/package/mqtt-match)
 * [sqlite3](https://www.npmjs.com/package/sqlite3)
+* [node-schedule](https://www.npmjs.com/package/node-schedule)
 
 ### Dependencies not yet added/implemented
 * [highcharts](https://www.npmjs.com/package/highcharts)
--- nodejs-mqtt-data-logger.js
+++ nodejs-mqtt-data-logger.js
@@ -5,30 +5,73 @@ var config = {
 	mqtt: 'mqtt://ha-01.localnet:1883',
 	topics: [
 		{ topic: 'shellies/+/relay/+/energy', type: 'energy', multiplier: 0.01, decimals: 3, unit: 'kWh', resetingCounter: true }
-	]
+	],
+	keep: {
+		// If integer, keep number of days.
+		// If false, don't aggregate into this time interval.
+		// If true, don't purge
+		raw: 100,
+		hourly: 500,
+		daily: true,
+	},
 }
 
 
-// Data logger
-var DL = {
+var DataLogger = {
+	schedule: null,
 	lastValues: {},	// Relevant for resetingCounters and to not write the same value twice in a row.
 	queue: [],
 	queueLock: false,
 
+	Init: function() {
+		if(config.keep.raw) {
+			DataLogger.schedule.scheduleJob('1 0 * * *', function() { DataLogger.HandleRawData() });
+		}
+		if(config.keep.hourly) {
+			DataLogger.schedule.scheduleJob('1 * * * *', function() { DataLogger.HandleHourlyData() });
+		}
+		if(config.keep.daily) {
+			DataLogger.schedule.scheduleJob('1 0 * * *', function() { DataLogger.HandleDailyData() });
+		}
+
+		DB.AggregateHourlyData();
+	},
+
+	HandleRawData: function() {
+		console.info('Handling raw data');
+		// Purge
+		if(config.keep.raw !== true) {
+			DB.DeleteOldRawData(config.keep.raw);
+		}
+	},
+	HandleHourlyData: function() {
+		// Aggregate
+		DB.AggregateHourlyData();
+
+		// Purge
+		// FIXME IMPLEMENT
+	},
+	HandleDailyData: function() {
+		// Aggregate
+		// FIXME IMPLEMENT
+
+		// Purge
+		// FIXME IMPLEMENT
+	},
+
 	AddIncomingMqttDataToQueue: function(topic, value) {
 		var job = { topic: topic, rawValue: value.toString() };
-		console.log('Adding job: ' + topic + ' => ' + value);
-		DL.queue.push(job);
+		console.info('Adding job: ' + topic + ' => ' + value);
+		DataLogger.queue.push(job);
 	},
 	TriggerQueue: function() {
-		if(DL.queueLock || !DL.queue.length) {
+		if(DataLogger.queueLock || !DataLogger.queue.length) {
 			return;
 		}
-		DL.queueLock = true;
+		DataLogger.queueLock = true;
 
-		job = DL.queue[0];
-		if(typeof(DL.lastValues[job.topic]) === 'undefined') {
-
+		job = DataLogger.queue[0];
+		if(typeof(DataLogger.lastValues[job.topic]) === 'undefined') {
 			let callback = function(topic, rawValue) {
 				return function(error, row) {
 					if(error) {
@@ -36,54 +79,54 @@ var DL = {
 						process.exit(1);
 					}
 					if(typeof(row) === 'undefined') {
-						console.log('New topic found: ' + topic + ' => ' + rawValue);
-						DL.lastValues[topic] = rawValue;
+						console.info('New topic found: ' + topic + ' => ' + rawValue);
+						DataLogger.lastValues[topic] = {rawValue: Number(rawValue), calculatedValue: 0};
 					} else {
-						console.log('Old topic restored: ' + topic + ' => ' + row.raw_value);
-						DL.lastValues[topic] = row.raw_value;
+						console.info('Old topic restored: ' + topic + ' => rawValue:' + row.raw_value + ' calculatedValue:' + row.calculated_value);
+						DataLogger.lastValues[topic] = {rawValue: Number(row.raw_value), calculatedValue: Number(row.calculated_value)};
 					}
 
-					DL.TriggerQueue();
+					DataLogger.TriggerQueue();
 				}
 			}
 			DB.GetLatestValueForTopic(job.topic, job.rawValue, callback);
-			DL.queueLock = false;
+			DataLogger.queueLock = false;
 			return;
 		}
 
-		if(job.rawValue != DL.lastValues[job.topic]) {
+		if(job.rawValue != DataLogger.lastValues[job.topic].rawValue) {
 			var match = MQTT.GetMatchingTopic(job.topic);
 
-			value = Number(job.rawValue);
-			if(value === 'NaN') {
-				DL.queueLock = false;
+			var calculatedValue = Number(job.rawValue);
+			if(calculatedValue === 'NaN') {
+				DataLogger.queueLock = false;
 				return false;
 			}
 
-			if(match.resetingCounter && job.rawValue < DL.lastValues[job.topic]) {
-				value = value + DL.lastValues[job.topic];
-			}
-
 			if(typeof(match.multiplier) !== 'undefined') {
-				value = value * match.multiplier;
+				calculatedValue = calculatedValue * match.multiplier;
 			}
 			if(typeof(match.decimals) !== 'undefined') {
-				value = value.toFixed(match.decimals);
+				calculatedValue = Number(calculatedValue.toFixed(match.decimals));
 			} else {
-				value = Number(value);
+				calculatedValue = Number(calculatedValue);
 			}
 
-			console.log('Writing to database: ' + job.topic + ' rawValue:' + job.rawValue + ' value: ' + value, ' lastRaw: ' + DL.lastValues[job.topic]);
-			DB.AddRawValue(job.topic, job.rawValue, value);
-			DL.lastValues[job.topic] = job.rawValue;
-			DL.queue.shift();
-			DL.queueLock = false;
+			if(match.resetingCounter && calculatedValue < DataLogger.lastValues[job.topic].calculatedValue) {
+				calculatedValue = calculatedValue + DataLogger.lastValues[job.topic].calculatedValue;
+			}
+
+			console.info('Writing to database: ' + job.topic + ' rawValue:' + job.rawValue + ' calculatedValue:' + calculatedValue, ' lastRaw:' + DataLogger.lastValues[job.topic].rawValue);
+			DB.AddRawValue(job.topic, job.rawValue, calculatedValue);
+			DataLogger.lastValues[job.topic] = {rawValue: Number(job.rawValue), calculatedValue: Number(calculatedValue)};
+			DataLogger.queue.shift();
+			DataLogger.queueLock = false;
 			return;
 		}
 
-		DL.queueLock = false;
+		DataLogger.queueLock = false;
 		return;
-	}
+	},
 }
 
 var MQTT = {
@@ -109,9 +152,9 @@ var MQTT = {
 
 		// Handle MQTT message callbacks
 		MQTT.mqttClient.on('message', function(topic, value) {
-			// DL.ParseData(topic, value.toString());
-			DL.AddIncomingMqttDataToQueue(topic, value);
-			DL.TriggerQueue();
+			// DataLogger.ParseData(topic, value.toString());
+			DataLogger.AddIncomingMqttDataToQueue(topic, value);
+			DataLogger.TriggerQueue();
 		});
 
 	},
@@ -130,14 +173,14 @@ var MQTT = {
 			}
 		});
 		return match;
-	}
-
+	},
 }
 
 var DB = {
 	sqlite3: null,
 	db: null,
 
+	// FIXME - Add (unique) indexes for topic, timestamp
 	dbSchema: `
 	CREATE TABLE IF NOT EXISTS raw_data (
 	  id INTEGER NOT NULL PRIMARY KEY,
@@ -146,6 +189,15 @@ var DB = {
 	  raw_value REAL NOT NULL,
 	  calculated_value REAL NOT NULL
 	);
+	CREATE UNIQUE INDEX IF NOT EXISTS rawdata_uidx on raw_data (topic, timestamp);
+
+	CREATE TABLE IF NOT EXISTS hourly_data (
+	  id INTEGER NOT NULL PRIMARY KEY,
+	  topic TEXT NOT NULL,
+	  timestamp NUMERIC NOT NULL,
+	  value REAL NOT NULL
+	);
+	CREATE UNIQUE INDEX IF NOT EXISTS hourlydata_uidx on hourly_data (topic, timestamp);
 	`,
 	Init: function() {
 		// Connecting to database
@@ -167,7 +219,7 @@ var DB = {
 		});
 	},
 	AddRawValue: function(topic, rawValue, value) {
-		let sql = "INSERT INTO raw_data (topic, timestamp, raw_value, calculated_value) VALUES (?, datetime(), ?, ?)";
+		let sql = "INSERT INTO raw_data (topic, timestamp, raw_value, calculated_value) VALUES (?, DATETIME(), ?, ?)";
 		DB.db.run(sql, [topic, rawValue, value], function(error) {
 			if(error) {
 				console.error(error);
@@ -176,11 +228,50 @@ var DB = {
 		});
 	},
 	GetLatestValueForTopic: function(topic, rawValue, callback) {
-		let sql = "SELECT topic, raw_value FROM raw_data WHERE topic = ? ORDER BY timestamp DESC LIMIT 1";
+		let sql = "SELECT topic, raw_value, calculated_value FROM raw_data WHERE topic = ? ORDER BY timestamp DESC LIMIT 1";
 		DB.db.get(sql, [topic], callback(topic, rawValue));
 
-	}
+	},
 
+	DeleteOldRawData: function(days) {
+		days = Number(days);
+		if(!days) {
+			console.error('raw days is 0');
+			process.exit(1);
+		}
+		let sql = "DELETE FROM raw_data WHERE timestamp < DATE(DATETIME('now', '-" + days + " day'))";
+		DB.db.run(sql, [], function(error) {
+			if(error) {
+				console.error(error);
+				process.exit(1);
+			}
+		});
+	},
+	AggregateHourlyData: function() {
+		// Get the first YYYY-MM-DD HH:00:00 that doesn't exist in table hourly_data
+		let sql = "SELECT STRFTIME('%Y-%m-%d %H:00:00', DATETIME(MAX(timestamp), '+1 hour')) AS timestamp FROM hourly_data";
+		DB.db.get(sql, [], function(error, row) {
+			if(error) {
+				console.error(error);
+				process.exit(1);
+			}
+			DB.AggregateHourlyDataFrom(row.timestamp);
+		});
+	},
+	AggregateHourlyDataFrom: function(timestamp) {
+		if(typeof(timestamp) === 'undefined') {
+			timestamp = '1970-01-01 00:00';
+		}
+		console.info('AggregateHourlyDataFrom: ' + timestamp)
+
+		let sql = "INSERT INTO hourly_data (topic, timestamp, value) SELECT topic, STRFTIME('%Y-%m-%d %H:00:00', DATETIME(timestamp, '1 hour')) AS ymdh, MAX(calculated_value) AS value FROM raw_data WHERE timestamp >= ? GROUP BY topic, ymdh";
+		DB.db.run(sql, [timestamp], function(error) {
+			if(error) {
+				console.error(error);
+				process.exit(1);
+			}
+		});
+	},
 }
 
 
@@ -189,10 +280,12 @@ var DB = {
 MQTT.mqtt = require('mqtt');
 MQTT.mqttMatch = require('mqtt-match');
 DB.sqlite3 = require('sqlite3').verbose();
+DataLogger.schedule = require('node-schedule');
 
 
 DB.Init();
 MQTT.Init();
+DataLogger.Init();
 
 
 
